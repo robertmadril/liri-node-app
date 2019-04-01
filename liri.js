8a3e130d2403f8ee980af8f6462ff3cc8e9a3b63
robertmadril <robertmmadril@gmail.com>
2019-03-31 20:35:12 -0700

    add structure to js files

--- liri.js
+++ liri.js
@@ -1,8 +1,45 @@
-require("dotenv").config();
+require("./.env").config();
 
 var keys = require("./keys.js");
 
 var command = process.argv[2];
 
+var userInput = "";
 
-console.log("working");
+function concertSearch(searchTerm) {
+
+};
+
+function spotifySearch(searchTerm) {
+
+};
+
+function movieSearch(searchTerm) {
+
+};
+
+function staticSearch(searchTerm) {
+
+};
+
+//control flow to determine what function is called.
+switch (command) {
+    case "concert-this": 
+        concertSearch(userInput);
+        break;
+
+    case "spotify-this-song": 
+        spotifySearch(userInput);
+        break;
+
+    case "movie-this": 
+        movieSearch(userInput);
+        break;
+    
+    case "do-what-it-says":
+        staticSearch(searchTerm);
+        break;
+
+    default:
+        console.log("Please enter a valid search term.");
+}
