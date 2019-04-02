4fcf1641040a17525e9f49a41aae5c1a2e56b687
robertmadril <robertmmadril@gmail.com>
2019-04-01 19:29:13 -0700

    add spotify functionality

--- liri.js
+++ liri.js
@@ -4,32 +4,39 @@ require('dotenv').config();
 //axios import
 var axios = require("axios");
 
-var Spotify = require('node-spotify-api');
-
 var keys = require("./keys.js");
 
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+ 
 var command = process.argv[2];
 
-var userInput = "";
+var userInput = process.argv.splice(3).toString();
+
+console.log(userInput);
 
 //functions that will handle API calls
 function concertSearch(searchTerm) {
 
     // Then run a request with axios to the OMDB API with the movie specified
-    var queryUrl = "rest.bandsintown.com/artists/" + searchTerm + "/events/";
+    var queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
 
     axios.get(queryUrl).then(
         function (response) {
-            console.log(response);
+            console.log(response.data[0]);
         }
     );
 };
 
 function spotifySearch(searchTerm) {
     spotify
-        .search({ type: 'track', query: searchTerm })
+        .search({ type: 'artist', query: searchTerm })
         .then(function (response) {
+
+            
             console.log(response);
+            
         })
         .catch(function (err) {
             console.log(err);
@@ -49,14 +56,14 @@ function movieSearch(searchTerm) {
 
 };
 
-function staticSearch(searchTerm) {
+function staticSearch() {
 
 };
 
 //control flow to determine what function is called.
 switch (command) {
     case "concert-this":
-        //concertSearch(userInput);
+        concertSearch(userInput);
         console.log("bands in town");
         break;
 
@@ -69,7 +76,7 @@ switch (command) {
         break;
 
     case "do-what-it-says":
-        staticSearch(searchTerm);
+        staticSearch();
         break;
 
     default:
