246ce199d829a55d7da549ebaaab1e8ffd87a691
robertmadril <robertmmadril@gmail.com>
2019-04-03 11:39:37 -0700

    add do-what-it-says functionality

--- liri.js
+++ liri.js
@@ -13,7 +13,7 @@ var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 
 var fs = require("fs");
- 
+
 var command = process.argv[2];
 
 var userInput = process.argv.splice(3).join("+").toString();
@@ -27,12 +27,12 @@ function concertSearch(searchTerm) {
     axios.get(queryUrl).then(
         function (response) {
             var objectReturned = response.data;
-            for (i=0; i<objectReturned.length; i++) {
-            console.log(objectReturned[i].venue.name);
-            console.log(objectReturned[i].venue.city);
-            console.log(moment(objectReturned[i].datetime).format("MM/DD/YYYY"));
-            console.log("__________________________");
-            
+            for (i = 0; i < objectReturned.length; i++) {
+                console.log(objectReturned[i].venue.name);
+                console.log(objectReturned[i].venue.city);
+                console.log(moment(objectReturned[i].datetime).format("MM/DD/YYYY"));
+                console.log("__________________________");
+
             }
         }
     );
@@ -44,13 +44,14 @@ function spotifySearch(searchTerm) {
         .search({ type: 'track', query: searchTerm })
         .then(function (response) {
 
-            
+
             console.log(response.tracks.items[0].artists[0].name);
             console.log(response.tracks.items[0].name);
             console.log(response.tracks.items[0].album.external_urls.spotify);
             console.log(response.tracks.items[0].album.name);
+            console.log("________________________");
 
-            
+
         })
         .catch(function (err) {
             console.log(err);
@@ -66,7 +67,7 @@ function movieSearch(searchTerm) {
 
     axios.get(queryUrl).then(
         function (response) {
-            console.log("Movie: " + response.data.Title);
+            console.log("Movie: " + response.data.Title)
             console.log("Year: " + response.data.Year);
             console.log("IMDB Rating: " + response.data.Ratings[0].Value);
             console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
@@ -74,61 +75,70 @@ function movieSearch(searchTerm) {
             console.log("Language: " + response.data.Language);
             console.log("Plot: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
+            console.log("________________________");
         }
     );
 
 };
 
 function staticSearch() {
-    fs.readFile("random.txt", "utf-8", function(err, data) {
+    fs.readFile("random.txt", "utf-8", function (err, data) {
         if (err) {
             return console.log(err);
-          }
-        
+        }
+
         var data = data.toString().split(",");
         command = data[0];
         userInput = data[1];
+        controlFlow();
     })
 };
 
 //control flow to determine what function is called.
 
-switch (command) {
+function controlFlow() {
+    switch (command) {
 
-    case "do-what-it-says":
-        staticSearch();
-        break;
+        case "do-what-it-says":
+            staticSearch();
+            break;
 
-    case "concert-this":
-        concertSearch(userInput);
-        break;
+        case "concert-this":
+            concertSearch(userInput);
+            break;
 
-    case "spotify-this-song":
-        if (!userInput) {
-        userInput = "Ace+of+Base"
-        spotifySearch(userInput); }
-        else {
-            spotifySearch(userInput);
-        }
-        break;
+        case "spotify-this-song":
+            if (!userInput) {
+                userInput = "Ace+of+Base"
+                spotifySearch(userInput);
+            }
+            else {
+                spotifySearch(userInput);
+            }
+            break;
 
-    case "movie-this":
-    if (!userInput) {
-        userInput = "Mr+Nobody"
-        movieSearch(userInput); }
-        else {
-            movieSearch(userInput);
-        }
-        break;
+        case "movie-this":
+            if (!userInput) {
+                userInput = "Mr+Nobody"
+                movieSearch(userInput);
+            }
+            else {
+                movieSearch(userInput);
+            }
+            break;
 
 
-    default:
-        console.log("Please enter a valid search term.");
+        default:
+            console.log("Please enter a valid search term.");
+    };
 };
+
+controlFlow();
+
 
 
 /* To-Do
 
-Create staticSearch from FS call (assign command and userInput)
+create log where responses are sent
 
 */
